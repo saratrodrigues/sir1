a592e2907486be4db67d1d5470a788ea32e9503d
saratrodrigues <sararodrigues2502@gmail.com>
2024-10-31 16:32:25 +0000

    1commit

--- public/euro.js
+++ public/euro.js
@@ -1,56 +1,70 @@
-
 document.addEventListener('DOMContentLoaded', function (e) {
     button = document.getElementById("genBtn");
     button.addEventListener('click', gerachave);
 
-    });
-
-function gerador (n, min, max){
+});
+    
+function gerador(n, min, max) {
     extracao = new Set();
-    while (extracao.size < n){
-        novo = Math.floor (Math.random() * (max - min + 1) + min);
-        extracao.add (novo); 
+
+    while (extracao.size < n) {
+        novo = Math.floor(Math.random() * (max - min + 1) + min);
+        extracao.add(novo);
     }
-    return [...extracao].sort((a, b) => a - b ); //funcao seta
+    return [...extracao].sort((a, b) =>  a - b);
 }
 
-function gerachaveJSON(){
-    let an = gerador (5, 1, 50);
-    let ae = gerador (2, 1, 12);
-     chaveJS = {
+function gerachaveJSON() {
+    let an = gerador(5, 1, 50);
+    let ae = gerador(2, 1, 12);
+
+    chaveJS = {
         numeros: an,
         estrelas: ae,
-        gerador: 'pedromoreira'
+        geradapor : 'pedromoreira'
     }
-    return JSON.stringify (chaveJS);
-} 
+    return JSON.stringify(chaveJS);
 
-function consomeChaveJSON(chavejson){
+}
+
+function consomeChaveJSON(chavejson) {
     let chaveJS = JSON.parse(chavejson);
     return chaveJS;
 }
 
-function gerachave(e) { 
-    let an = gerador (5, 1, 50);
-    let ae = gerador (2, 1, 12);
+function gerachave(e) {
 
-    console.log (gerador (5, 1, 50));
-    console.log("vou gerar a chave", e);
+    fetch('https://sir1-3a5o.onrender.com/euro')
+        .then((res => res.json()))
+        .then((data) => {
+            updatechave(data);
+        });
+        
+}
 
+function updatechave(chave) {
+    console.log(chave)
+
+    //let an = gerador(5, 1, 50);
+    //let ae = gerador(2, 1, 12);
+
+    an = chave.numeros;
+    ae = chave.estrelas;
+
+    
     //debugger;
-
     listanumeros = document.getElementById("olMain");
     listanumeros.innerHTML = "";
-    an.forEach((numero) => { //an é array de numeros
-        let newli = document.createElement ("li");
+    an.forEach((numero) => {
+        let newli = document.createElement("li");
         newli.innerHTML = numero;
         listanumeros.appendChild(newli);
     })
 
     listaestrelas = document.getElementById("olStars");
     listaestrelas.innerHTML = "";
-    ae.forEach((estrela) => { //ae é array de estrelas
-        let newli = document.createElement ("li");
+    ae.forEach((estrela) => {
+        let newli = document.createElement("li");
         newli.innerHTML = estrela;
         listaestrelas.appendChild(newli);
     })
